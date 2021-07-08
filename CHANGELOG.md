23953aa06b3cec527c32c7f694d5d4f13fe3163e
SaeidR <saeidr@evenset.com>
2021-07-08 15:17:08 -0400

    DHP-3: models and migrations added

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 0.0.1-dev-2-feature/DHP-3-models-mogrations
+- Models and migrations added
+
 ## 0.0.1-dev-2-feature/DHP-18-linting
 - Linting added
 
--- advanced_survey/models.py
+++ advanced_survey/models.py
@@ -1,2 +1,74 @@
 """Models"""
-# from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
+
+def get_field_types():
+    return (
+        ('LineEdit', 'Line Edit'),
+        ('TextArea', 'Text Area'),
+        ('Select', 'Select'),
+        ('Checkbox', 'Checkbix'),
+        ('Radiobox', 'Radiobox'),
+        ('Email', 'Email'),
+        ('URL', 'URL'),
+        ('Phone', 'Phone'),
+        ('Boolean', 'Boolean'),
+        ('Numeric', 'Numeric'),
+        ('File', 'File'),
+        ('Image', 'Image'),
+        ('Date', 'Date'),
+        ('Time', 'Time'),
+        ('Rating', 'Rating')
+    )
+
+class Survey(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expire_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Question(models.Model):
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE, db_index=True)
+    page = models.IntegerField(default=0, db_index=True)
+    list_order = models.IntegerField(default=0, db_index=True)
+    question = models.TextField()
+    comment = models.TextField(null=True, blank=True)
+    field_type = models.CharField(max_length=255, choices=get_field_types())
+    options_url = models.URLField(null=True, blank=True)
+    options = models.TextField(null=True, blank=True)
+    is_required = models.CharField(max_length=255, null=True, blank=True)
+    is_visible = models.CharField(max_length=255, null=True, blank=True)
+    validation_rules = models.CharField(max_length=255, null=True, blank=True)
+
+    @property
+    def required(self):
+        if self.is_required is None:
+            return False
+        return self.is_required.split("|")
+
+    @property
+    def visible(self):
+        if self.is_visible is None:
+            return True
+        return self.is_visible.split("|")
+
+    @property
+    def rules(self):
+        return self.validation_rules.split("|")
+
+    def __str__(self):
+        return self.question
+
+class Answer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE, db_index=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, db_index=True)
+    answer = models.TextField()
+    session = models.CharField(max_length=255, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.answer
