10287b6b31988f1fc6138220f8cf1b24b0270788
David Walker <davidw@evenset.com>
2021-07-14 10:17:12 -0400

    add stub component

---
+++ advanced_survey/static/advanced-survey/src/direflow-components/is-visible/is-visible.tsx
@@ -0,0 +1,24 @@
+import React, {useState, useCallback, ChangeEvent, useContext} from 'react';
+import {StateContext} from '../state'
+type Props = {
+  questionId?: string;
+  previousQuestionIds?: string;
+}
+const inline = {display: "inline"}
+const VisibleSelector: React.FC<Props> = () => {
+  const {state, dispatch} = useContext
+  return (
+    <span style={inline}>
+      visible <select className="form-control" style={inline}>
+          <option key="always">Always</option>
+          <option key="if">If</option>
+          <option key="unless">Unless</option>
+      </select>
+      {/* previous question ?*/}
+      {/* is answered/is answered with ?*/}
+      {/* option selection ?*/}
+    </span>
+  )
+
+}
+export default VisibleSelector;
