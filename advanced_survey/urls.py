5d8b21687465aab4e28d13721061472746df906a
SaeidR <saeidr@evenset.com>
2021-07-20 14:06:58 -0400

    DHP-21: surveyjs format added

--- advanced_survey/urls.py
+++ advanced_survey/urls.py
@@ -1,8 +1,15 @@
 """Routes"""
 from django.urls import path
-from .views import save_survey, get_survey
+from .views import (
+    save_survey,
+    get_survey,
+    get_surveyjs,
+    save_answer
+)
 
 urlpatterns = [
     path('save/', save_survey, name="advanced_survey_save_url"),
     path('get/', get_survey, name="advanced_survey_get_survey"),
+    path('surveyjs/', get_surveyjs, name="advanced_survey_get_surveyjs"),
+    path('answer/', save_answer, name="advanced_survey_save_answer")
 ]
